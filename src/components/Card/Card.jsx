8ae1e438aac637a01a045358f677e546fbc9407f
maduxt <maria.eduarda2812@hotmail.com>
2024-03-17 15:00:13 -0300

    feat card

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,34 +1,74 @@
 import React, { useState } from 'react'
-import {AnimateSharedLayout} from 'framer-motion'
+import './card.css'
+import { CircularProgressbar } from 'react-circular-progressbar'
+import 'react-circular-progressbar/dist/styles.css'
+import { LayoutGroup } from 'framer-motion'
+import Chart from 'react-apexcharts'
+import { IoMdClose } from "react-icons/io"
+import { DataChart } from '../../Data/Data'
+
 
 const Card = (props) => {
 
 const [expanded, setExpanded] = useState(false)
 
   return (
-    <AnimateSharedLayout>
+    <LayoutGroup>
         {
-            expanded? (
-                'Expanded'
-            ):
-            <CompactCard param = {props} />
+             expanded?
+                 <ExpandedCard param={props} setExpanded={()=>setExpanded(false)}/>:
+                 <CompactCard param = {props} setExpanded={()=>setExpanded(true)} />
         }
-    </AnimateSharedLayout>
+    </LayoutGroup>
   )
 }
 
-function CompactCard ({param}){
-    const png = param.png;
+function CompactCard ({param, setExpanded}){
+
+    const Png = param.png;
+        return(
+            <div className="CompactCard"
+            style={{
+                background: param.color.backGround,
+                boxShadow: param.color.boxShadow
+            }}
+            onClick={setExpanded}
+            >
+                <div className="radialBar">
+                    <CircularProgressbar
+                    value={param.barValue}
+                    text={param.barValue}
+                    />
+                    <span>{param.title}</span>
+                </div>
+                <div className="detail">
+                    < Png />
+                    <span>${param.value}</span>
+                    <span>Últimas 24h horas.</span>
+                </div>
+            </div>
+        )
+    }
+
+function ExpandedCard({param, setExpanded}){
     return(
-        <div className="CompactCard">
-            <div className="radialBar">
-                Chart
+        <div className="ExpandedCard"
+        style={{
+        background: param.color.backGround,
+        boxShadow: param.color.boxShadow,
+    }}
+    layout
+        >
+            <div>
+            <IoMdClose onClick={setExpanded} />
             </div>
-            <div className="detail">
-                <png/>
-                <span>${param.value}</span>
-                <span>últimas 24h horas.</span>
+            <span>
+                {param.title}
+            </span>
+            <div className="chartContainer">
+                <Chart series={param.series} type='area' options={DataChart.options}/>
             </div>
+            <span>Últimas 24 horas</span>
         </div>
     )
 }
